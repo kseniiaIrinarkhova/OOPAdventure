9906ac509d500a24491894c24ce79e5de6956bdc
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-02 13:18:02 -0800

    added nested objects

--- script.js
+++ script.js
@@ -5,7 +5,18 @@
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
+    inventory: ["sword", "potion", "artifact"],
+    //nested object
+    companion: {
+        name: "Leo",
+        type: "Cat",
+        //companion's friend
+        companion:{
+            name: "Frank",
+            type: "Flea",
+            inventory: ["small hat","sunglasses"]
+        }
+    }
 }
 //log the 1st inventory object
 console.log(adventurer.inventory[0]);
