33ca387d4cc9e24a103a6bc0aa8a85f59c46d95d
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-02 14:22:46 -0800

    added static propeties for classes

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 /************************************
 *****Part 1: Humble Beginnings*******
 *************************************/
-
+console.log(`
+*************************************
+****************Part 1***************
+*************************************`
+);
 //prepared objects
 const adventurer_obj = {
     name: "Robin",
@@ -12,10 +16,10 @@ const adventurer_obj = {
         name: "Leo",
         type: "Cat",
         //companion's friend
-        companion:{
+        companion: {
             name: "Frank",
             type: "Flea",
-            inventory: ["small hat","sunglasses"]
+            inventory: ["small hat", "sunglasses"]
         }
     },
     //method for the object
@@ -27,7 +31,7 @@ const adventurer_obj = {
 //log the 1st inventory object
 console.log(adventurer_obj.inventory[0]);
 //show all inventory:
-adventurer_obj.inventory.forEach((item) =>{console.log(`${adventurer_obj.name} has ${item}`)});
+adventurer_obj.inventory.forEach((item) => { console.log(`${adventurer_obj.name} has ${item}`) });
 
 //show an object
 console.log(adventurer_obj)
@@ -40,8 +44,15 @@ adventurer_obj.roll();
 /********************************************* 
  * *******Part 2: Class Fantasy***************
 **********************************************/
+console.log(`
+*************************************
+****************Part 2***************
+*************************************`
+);
 //main class
 class Character {
+    //add static properties
+    static MAX_HEALTH = 100
     constructor(name) {
         this.name = name;
         this.health = 100;
@@ -54,10 +65,10 @@ class Character {
         const result = Math.floor(Math.random() * 20) + 1 + mod;
         console.log(`${this.name} rolled a ${result}.`)
     }
-    addCompanions(...companion){
+    addCompanions(...companion) {
         this.companions.push(...companion)
     }
-    addToInventory(...items){
+    addToInventory(...items) {
         this.inventory.push(...items)
     }
 }
@@ -81,14 +92,25 @@ robin_character.companion.companion.roll();
 /********************************************* 
  * *******Part 3: Class Features**************
 **********************************************/
+console.log(`
+*************************************
+****************Part 4***************
+*************************************`
+);
 //inheritance example
 class Adventurer extends Character {
+    static ROLES = ["Fighter", "Healer", "Wizard"]
     constructor(name, role) {
         super(name);
         // Adventurers have specialized roles.
-        this.role = role;
+        if (Adventurer.ROLES.includes(role)) { this.role = role; }
+        else {
+            console.log(`Roles should be from Adventurer.ROLES list. Cannot define ${role}`);
+            this.type = undefined
+        }
+
         // Every adventurer starts with a bed and 50 gold coins.
-        this.inventory.push("bedroll", "50 gold coins");        
+        this.inventory.push("bedroll", "50 gold coins");
     }
     // Adventurers have the ability to scout ahead of them.
     scout() {
@@ -96,26 +118,32 @@ class Adventurer extends Character {
         super.roll();
     }
     //adventurer info
-    toString(){
+    toString() {
         return `${this.name} is ${this.role}. Health: ${this.health}. Inventory: ${this.inventory}.`
     }
 }
 
-class Companion extends Character{
+class Companion extends Character {
+    static TYPES = ["Cat", "Flea", "Dog", "Horse"]
     constructor(name, type) {
         super(name);
         // Companions have specialized types.
-        this.type = type;        
+        if (Companion.TYPES.includes(type)) { this.type = type; }
+        else {
+            console.log(`Types should be from Companion.TYPES list. Cannot define ${type}`);
+            this.type = undefined
+        }
+
     }
-    toString(){
+    toString() {
         return `Companion ${this.name} is ${this.type}. Health: ${this.health}`
     }
 }
 
-const robin = new Adventurer("Robin","archer");
+const robin = new Adventurer("Robin", "archer");
 //robin.inventory.push("sword", "potion", "artifact");
 robin.addToInventory("sword", "potion", "artifact");
-const leo = new Companion("Leo","Cat")
+const leo = new Companion("Leo", "Cat")
 //robin.companions.push(leo);
 robin.addCompanions(leo)
 const frank = new Companion("Frank", "Flea")
@@ -127,3 +155,22 @@ leo.addCompanions(frank)
 console.log(robin)
 console.log(robin.companions)
 console.log(leo.companions)
+
+/********************************************* 
+ * *******Part 4: Class Uniforms**************
+**********************************************/
+console.log(`
+*************************************
+****************Part 4***************
+*************************************`
+);
+//create new objects using static properties
+const newRobin = new Adventurer("Robin", Adventurer.ROLES[2]);
+newRobin.addToInventory("sword", "potion", "artifact");
+const newLeo = new Companion("Leo", Companion.TYPES[0]);
+newRobin.addCompanions(newLeo)
+const newFrank = new Companion("Frank", Companion.TYPES[1])
+newFrank.addToInventory("small hat", "sunglasses");
+newLeo.addCompanions(newFrank);
+
+console.log(newRobin)
