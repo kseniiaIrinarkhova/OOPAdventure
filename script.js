9bcb7a3e7ae2227dff29291d1a5ba4acb5f7f456
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-02 13:24:17 -0800

    preparation for 2 part

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-/************************************ */
-/**Part 1: Humble Beginnings */
+/************************************
+*****Part 1: Humble Beginnings*******
+*************************************/
 
 //prepared objects
-const adventurer = {
+const adventurer_obj = {
     name: "Robin",
     health: 10,
     inventory: ["sword", "potion", "artifact"],
@@ -24,14 +25,18 @@ const adventurer = {
     }
 }
 //log the 1st inventory object
-console.log(adventurer.inventory[0]);
+console.log(adventurer_obj.inventory[0]);
 //show all inventory:
-adventurer.inventory.forEach((item) =>{console.log(`${adventurer.name} has ${item}`)});
+adventurer_obj.inventory.forEach((item) =>{console.log(`${adventurer_obj.name} has ${item}`)});
 
 //show an object
-console.log(adventurer)
+console.log(adventurer_obj)
 
 //call object's method
-adventurer.roll();
-adventurer.roll();
-adventurer.roll();
+adventurer_obj.roll();
+adventurer_obj.roll();
+adventurer_obj.roll();
+
+/********************************************* 
+ * *******Part 2: Class Fantasy***************
+**********************************************/
